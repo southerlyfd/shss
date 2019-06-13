cd2bd7e49475730e4e7b644def9e2012b514f6c5
jianghongbo <southerlyfd@yeah.net>
2019-06-13 18:03:05 +0800

    启动Banner自定义

--- shss/src/main/java/com/jianghongbo/ShssApplication.java
+++ shss/src/main/java/com/jianghongbo/ShssApplication.java
@@ -1,5 +1,6 @@
 package com.jianghongbo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
@@ -10,7 +11,20 @@ import org.springframework.context.annotation.ComponentScan;
 public class ShssApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShssApplication.class, args);
+
+		/**
+		 * 隐藏banner启动方式
+		 */
+		SpringApplication springApplication = new SpringApplication(ShssApplication.class);
+		//设置banner的模式为隐藏
+		springApplication.setBannerMode(Banner.Mode.CONSOLE);
+		//启动springboot应用程序
+		springApplication.run(args);
+
+		/**
+		 * 原启动方式
+		 */
+		/*SpringApplication.run(ShssApplication.class, args);*/
 	}
 
 }
